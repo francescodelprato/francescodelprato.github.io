fb4c6bf0462844f839b7669b4a4abe3ce87e5742
Francesco Del Prato <102439534+francescodelprato@users.noreply.github.com>
2024-11-13 13:52:55 +0100

    Website's first commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const metaData = {
+  baseUrl: "https://francescodelprato.github.io/",
+  title: "Francesco Del Prato",
+  name: "Francesco Del Prato",
+  ogImage: "/opengraph-image.png",
+  description:
+    "The personal website of Francesco Del Prato",
+};
+
+export const socialLinks = {
+  twitter: "https://x.com/fdelprato",
+  linkedin: "https://www.linkedin.com/in/francesco-del-prato/",
+  email: "mailto:francesco.delprato@econ.au.dk",
+};
