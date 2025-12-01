e9ab59f01907655e1b963f2968a19f639e16a099
Francesco Del Prato <102439534+francescodelprato@users.noreply.github.com>
2025-12-01 16:53:12 +0100

    New version

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/blog/getting-started.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fgetting-started.mdx&astroContentModuleFlag=true")],
+["src/content/blog/custom-mdx-examples.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fcustom-mdx-examples.mdx&astroContentModuleFlag=true")]]);
+		
